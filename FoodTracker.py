af90dd654fcf53f3ad927e3a2fa42688a92fe6a7
hbl4 <hbl4@kent.ac.uk>
2026-03-10 01:42:40 +0000

    added inheritence and use of classes

--- FoodTracker.py
+++ FoodTracker.py
@@ -2,4 +2,18 @@ import re
 import csv
 from datetime import datetime 
 
+class AccountInfo: 
+    def _init_(self, username, password):
+        if not  username:
+            raise ValueError ("Missing Username ")
+        if not password:
+            raise ValueError (" Missing Password")
+        self.username = username 
+        self.password = password
+
+class user(AccountInfo):
+    def _init_(self, username, password):
+        super()._init_(username,password)
+
+
 
